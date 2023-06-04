9ff856403ef9b507f1fac0a9238e07069750e12c
MarcoAZ <marcoaz2001@outlook.com>
2023-06-04 21:57:18 +0200

    estructura de datos para los ataques y respectivos inner

--- js/systemon.js
+++ js/systemon.js
@@ -1,6 +1,73 @@
+let playerAtack
+let monPlayer
+
+// Atacks
+function blazeBall() {
+    return "Blaze Ball!"
+}
+function igniteSpeed() {
+    return "Ignite Speed!"
+}
+function cuttingWind() {
+    return "Cutting Wind!"
+}
+function hypnoticBuzz() {
+    return "Hypnotic Buzz!"
+}
+function danceOfFlames() {
+    return "Dance of Flames!"
+}
+function earthQuake() {
+    return "Earthquake!"
+}
+function thunderStrike() {
+    return "Thunder Strike!"
+}
+function tsunami() {
+    return "Tsunami!"
+}
+function aura() {
+    return "Aura!"
+}
+function infernoBurst() {
+    return "Inferno Burst!"
+}
+function timeWarp() {
+    return "Time Warp!"
+}
+function gravityShift() {
+    return "Gravity Shift!"
+}
+function cyclone() {
+    return "Cyclone!"
+}
+function hydroVortex() {
+    return "Hydro Vortex!"
+}
+function callOfTheSea() {
+    return "Call of the Sea!"
+}
+
 function initGame() {
+
+    // Selection of the Systemon
     let buttonMon = document.getElementById("button_mon")
-    buttonMon.addEventListener("click", selectMonPlayer)
+    buttonMon.addEventListener("click", function () {monPlayer = selectMonPlayer();})
+    console.log(monPlayer)
+
+    let spanMonPlayerAtack1 = document.getElementById("span_atack1")
+    nameAtack = getAttackFunction(monPlayer, 'attack1');
+    spanMonPlayerAtack1.innerHTML = nameAtack
+
+    // Atacks && Defenses
+    let buttonAttack1 = document.getElementById("button_attack1");
+    buttonAttack1.addEventListener("click", function() {atack(monPlayer, "attack1"); })
+
+    let buttonAttack2 = document.getElementById("button_atack2")
+    buttonAttack2.addEventListener("click", function() {atack(monPlayer, "attack2"); })
+
+    let buttonAttack3 = document.getElementById("button_atack3")
+    buttonAttack3.addEventListener("click", function() {atack(monPlayer, "attack3"); })
 }
 
 function selectMonPlayer() {
@@ -39,7 +106,61 @@ function selectMonPlayer() {
     alert("You have selected a Systemon from " + namePlanet + "!")
     
     selectMonComputer()
+    return (namePlanet)
 }
+
+function atack (monPlayer, atackType) {
+    playerAtack = getAttackFunction(monPlayer, atackType)
+    console.log(playerAtack)
+    return playerAtack
+}
+
+function getAttackFunction(monPlayer, attackType) {
+    const alienAttacks = {
+    Mercury: {
+        attack1: blazeBall,
+        attack2: igniteSpeed,
+        attack3: cuttingWind
+      },
+    Venus: {
+        attack1: hypnoticBuzz,
+        attack2: danceOfFlames,
+        attack3: igniteSpeed
+      },
+    Earth: {
+        attack1: earthQuake,
+        attack2: thunderStrike,
+        attack3: tsunami
+      },
+    Mars: {
+        attack1: thunderStrike,
+        attack2: aura,
+        attack3: infernoBurst
+        },
+    Saturn: {
+        attack1: timeWarp,
+        attack2: gravityShift,
+        attack3: cyclone
+        },
+    Neptune: {
+        attack1: tsunami,
+        attack2: hydroVortex,
+        attack3: callOfTheSea
+        }
+
+    };
+
+    if (alienAttacks[monPlayer][attackType]) {
+        alert("You have selected " + attackType + "!")
+        console.log("You have selected " + attackType + "!");
+        return alienAttacks[monPlayer][attackType];
+      } else {
+        console.log("Función de ataque no encontrada");
+        return function () {
+          // Function for default erroor management
+        };
+      }
+    }
 
 function selectMonComputer() {
     let mons = ["Mercurion", "Venusylph", "Terrathor", "Martidrone", "Saturchron", "Nepsus"]
@@ -47,7 +168,6 @@ function selectMonComputer() {
     let monComputer = mons[random(0, 6)]
     spanMonComputer.innerHTML = monComputer
     alert("The computer has selected a " + monComputer + "!")
-    battle()
 }
 
 function random(min, max) {
