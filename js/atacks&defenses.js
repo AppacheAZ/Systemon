f66e2c4ba29d5d4adc1b41966686dbd9d17d234b
MarcoAZ <marcoaz2001@outlook.com>
2023-06-06 04:07:35 +0200

    Separo las funciones de ataque y defensa en otro archivo, añado un add event listener para los ataques

---
+++ js/atacks&defenses.js
@@ -0,0 +1,53 @@
+function blazeBall(option) {
+    if (option == "nameAtack") {
+        return "Blaze Ball 🔥"
+    }
+    else if (option == "damage") {
+        return 10
+    }
+    else if (option == "type") {
+        return "fire"
+    }
+}
+function igniteSpeed() {
+    return "Ignite Speed ❤️‍🔥"
+}
+function cuttingWind() {
+    return "Cutting Wind 💨"
+}
+function hypnoticBuzz() {
+    return "Hypnotic Buzz 🌀"
+}
+function danceOfFlames() {
+    return "Dance of Flames 🔥"
+}
+function earthQuake() {
+    return "Earthquake 🌋"
+}
+function thunderStrike() {
+    return "Thunder Strike ⚡"
+}
+function tsunami() {
+    return "Tsunami 🌊"
+}
+function aura() {
+    return "Aura 🌀"
+}
+function infernoBurst() {
+    return "Inferno Burst 🔥"
+}
+function timeWarp() {
+    return "Time Warp ⏳"
+}
+function gravityShift() {
+    return "Gravity Shift 🌌"
+}
+function cyclone() {
+    return "Cyclone 🌀"
+}
+function hydroVortex() {
+    return "Hydro Vortex 💧"
+}
+function callOfTheSea() {
+    return "Call of the Sea 🌊"
+}
--- js/systemon.js
+++ js/systemon.js
@@ -1,76 +1,46 @@
+
 let playerAtack
 let monPlayer
 
-// Atacks
-function blazeBall() {
-    return "Blaze Ball!"
-}
-function igniteSpeed() {
-    return "Ignite Speed!"
-}
-function cuttingWind() {
-    return "Cutting Wind!"
-}
-function hypnoticBuzz() {
-    return "Hypnotic Buzz!"
-}
-function danceOfFlames() {
-    return "Dance of Flames!"
-}
-function earthQuake() {
-    return "Earthquake!"
-}
-function thunderStrike() {
-    return "Thunder Strike!"
-}
-function tsunami() {
-    return "Tsunami!"
-}
-function aura() {
-    return "Aura!"
-}
-function infernoBurst() {
-    return "Inferno Burst!"
-}
-function timeWarp() {
-    return "Time Warp!"
-}
-function gravityShift() {
-    return "Gravity Shift!"
-}
-function cyclone() {
-    return "Cyclone!"
-}
-function hydroVortex() {
-    return "Hydro Vortex!"
-}
-function callOfTheSea() {
-    return "Call of the Sea!"
-}
 
 function initGame() {
 
     // Selection of the Systemon
     let buttonMon = document.getElementById("button_mon")
-    buttonMon.addEventListener("click", function () {monPlayer = selectMonPlayer();})
-    console.log(monPlayer)
+    buttonMon.addEventListener("click", function () {
+        selectMonPlayer(function(selectedMon) {
+            monPlayer = selectedMon;
+            console.log(monPlayer);
+            
+            // Change the name of the Systemons atacks
+            let spanMonPlayerAtack1 = document.getElementById("span_atack1")
+            nameAtack = getAttackFunction(monPlayer, 'attack1');
+            spanMonPlayerAtack1.innerHTML = nameAtack;
+            let spanMonPlayerAtack2 = document.getElementById("span_atack2")
+            nameAtack = getAttackFunction(monPlayer, 'attack2');
+            spanMonPlayerAtack2.innerHTML = nameAtack;
+            let spanMonPlayerAtack3 = document.getElementById("span_atack3")
+            nameAtack = getAttackFunction(monPlayer, 'attack3');
+            spanMonPlayerAtack3.innerHTML = nameAtack;
+        });
+    });
 
-    let spanMonPlayerAtack1 = document.getElementById("span_atack1")
-    nameAtack = getAttackFunction(monPlayer, 'attack1');
-    spanMonPlayerAtack1.innerHTML = nameAtack
+    
 
     // Atacks && Defenses
-    let buttonAttack1 = document.getElementById("button_attack1");
-    buttonAttack1.addEventListener("click", function() {atack(monPlayer, "attack1"); })
+    let button_atack1 = document.getElementById("button_atack1");
+    button_atack1.addEventListener("click", function() {atack(monPlayer, "attack1"); })
 
-    let buttonAttack2 = document.getElementById("button_atack2")
-    buttonAttack2.addEventListener("click", function() {atack(monPlayer, "attack2"); })
+    let button_atack2 = document.getElementById("button_atack2")
+    button_atack2.addEventListener("click", function() {atack(monPlayer, "attack2"); })
 
-    let buttonAttack3 = document.getElementById("button_atack3")
-    buttonAttack3.addEventListener("click", function() {atack(monPlayer, "attack3"); })
+    let button_atack3 = document.getElementById("button_atack3")
+    button_atack3.addEventListener("click", function() {atack(monPlayer, "attack3"); })
 }
 
-function selectMonPlayer() {
+function selectMonPlayer(callback) {
+
+    // Selection of the Systemon and change the span with the name of the Systemon
     namePlanet = ""
     let inputMer = document.getElementById("Mer")
     let inputVen = document.getElementById("Ven")
@@ -106,7 +76,8 @@ function selectMonPlayer() {
     alert("You have selected a Systemon from " + namePlanet + "!")
     
     selectMonComputer()
-    return (namePlanet)
+    callback (namePlanet)
+    return namePlanet
 }
 
 function atack (monPlayer, atackType) {
@@ -118,34 +89,34 @@ function atack (monPlayer, atackType) {
 function getAttackFunction(monPlayer, attackType) {
     const alienAttacks = {
     Mercury: {
-        attack1: blazeBall,
-        attack2: igniteSpeed,
-        attack3: cuttingWind
+        attack1: blazeBall("nameAtack"),
+        attack2: igniteSpeed("nameAtack"),
+        attack3: cuttingWind("nameAtack")
       },
     Venus: {
-        attack1: hypnoticBuzz,
-        attack2: danceOfFlames,
-        attack3: igniteSpeed
+        attack1: hypnoticBuzz("nameAtack"),
+        attack2: danceOfFlames("nameAtack"),
+        attack3: igniteSpeed("nameAtack")
       },
     Earth: {
-        attack1: earthQuake,
-        attack2: thunderStrike,
-        attack3: tsunami
+        attack1: earthQuake("nameAtack"),
+        attack2: thunderStrike("nameAtack"),
+        attack3: tsunami("nameAtack")
       },
     Mars: {
-        attack1: thunderStrike,
-        attack2: aura,
-        attack3: infernoBurst
+        attack1: thunderStrike("nameAtack"),
+        attack2: aura("nameAtack"),
+        attack3: infernoBurst("nameAtack")
         },
     Saturn: {
-        attack1: timeWarp,
-        attack2: gravityShift,
-        attack3: cyclone
+        attack1: timeWarp("nameAtack"),
+        attack2: gravityShift("nameAtack"),
+        attack3: cyclone("nameAtack")
         },
     Neptune: {
-        attack1: tsunami,
-        attack2: hydroVortex,
-        attack3: callOfTheSea
+        attack1: tsunami("nameAtack"),
+        attack2: hydroVortex("nameAtack"),
+        attack3: callOfTheSea("nameAtack")
         }
 
     };
@@ -153,7 +124,8 @@ function getAttackFunction(monPlayer, attackType) {
     if (alienAttacks[monPlayer][attackType]) {
         alert("You have selected " + attackType + "!")
         console.log("You have selected " + attackType + "!");
-        return alienAttacks[monPlayer][attackType];
+        atackname = alienAttacks[monPlayer][attackType]
+        return atackname;
       } else {
         console.log("Función de ataque no encontrada");
         return function () {
@@ -173,5 +145,6 @@ function selectMonComputer() {
 function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
 
 window.addEventListener("load", initGame)
